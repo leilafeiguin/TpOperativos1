0b27eacbb39e7a8315d617c52f045af1aa0aee18
marcoporracin <marcoporracin@gamil.com>
2017-05-21 17:44:08 -0300

    Se arrgla la conexion inicial, falta handshake para checkpoint 1

--- Kernel/Debug/src/subdir.mk
+++ Kernel/Debug/src/subdir.mk
@@ -17,7 +17,7 @@ C_DEPS += \
 src/Kernel.o: ../src/Kernel.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -I"/home/utnso/workspace/Libraries" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"src/Kernel.d" -o "$@" "$<"
+	gcc -I"/home/utnso/workspace/tp-2017-1c-AsadoClash/Libraries" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"src/Kernel.d" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
