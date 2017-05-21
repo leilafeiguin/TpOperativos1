7bc36cbc5e4f99b72a24b24a1ac1cd509dd5f4ca
marcoporracin <marcoporracin@gamil.com>
2017-05-21 15:11:48 -0300

    Agregamos librerias de manejo de sockets

---
+++ Libraries/socketConfig.h
@@ -0,0 +1,89 @@
+/*
+ * socketConfig.h
+ *
+ *  Created on: 6/5/2017
+ *      Author: utnso
+ */
+
+#ifndef SOCKETCONFIG_H_
+#define SOCKETCONFIG_H_
+
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdint.h>
+#include <errno.h>
+#include <stdbool.h>
+#include <string.h>
+#include <commons/string.h>
+#include <commons/error.h>
+#include <commons/config.h>
+
+
+typedef int un_socket;
+typedef struct {
+	int codigo_operacion;
+	int tamanio;
+	void * data;
+} t_paquete;
+
+/**	@NAME: conectar_a
+ * 	@DESC: Intenta conectarse.
+ * 	@RETURN: Devuelve el socket o te avisa si hubo un error al conectarse.
+ *
+ */
+
+un_socket conectar_a(char *IP, char* Port);
+
+/**	@NAME: socket_escucha
+ * 	@DESC: Configura un socket que solo le falta hacer listen.
+ * 	@RETURN: Devuelve el socket listo para escuchar o te avisa si hubo un error al conectarse.
+ *
+ */
+
+un_socket socket_escucha(char* IP, char* Port);
+
+/**	@NAME: enviar
+ * 	@DESC: Hace el envio de la data que le pasamos. No hay que hacer más nada.
+ *
+ */
+
+void enviar(un_socket socket_envio, int codigo_operacion, int tamanio,
+		void * data);
+
+/**	@NAME: recibir
+ * 	@DESC: devuelve un paquete que está en el socket pedido
+ *
+ */
+t_paquete* recibir(un_socket socket_para_recibir);
+
+/**	@NAME: aceptar_conexion
+ * 	@DESC: devuelve el socket nuevo que se quería conectar
+ *
+ */
+un_socket aceptar_conexion(un_socket socket_escuchador);
+
+/**	@NAME: liberar_paquete
+ * 	@DESC: libera el paquete y su data.
+ *
+ */
+void liberar_paquete(t_paquete * paquete);
+
+/**	@NAME: realizar_handshake
+ *
+ */
+bool realizar_handshake(un_socket socket_del_servidor);
+
+/**	@NAME: esperar_handshake
+ *
+ */
+bool esperar_handshake(un_socket socket_del_cliente);
+
+char get_campo_config_char(t_config* archivo_configuracion, char* nombre_campo);
+
+int get_campo_config_int(t_config* archivo_configuracion, char* nombre_campo);
+
+int get_campo_config_array(t_config* archivo_configuracion, char* nombre_campo);
+
+#endif /* SOCKETCONFIG_H_ */
